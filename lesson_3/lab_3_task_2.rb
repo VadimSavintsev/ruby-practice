ff2ca16101ca0465a9fff094f1749cd9ac3fe1a4
VadimSavintsev <161233776+VadimSavintsev@users.noreply.github.com>
2024-07-09 09:44:09 +0200

    Added final version for task_2

--- lesson_3/lab_3_task_2.rb
+++ lesson_3/lab_3_task_2.rb
@@ -5,51 +5,52 @@
 #Программа завершается выводом на экран содержимого файла results.txt построчно, если все студенты из первого файла были записаны во второй
 #Или если пользователь ввёл с клавиатуры -1.
 
-stud_info='students.txt'
-buffer='buffer.txt'
-result='results.txt'
+BD_FILE='students.txt'
+BUFFER = 'buffer.txt'
+RESULT = 'results.txt'
 
-def Updates(num)
-    file=File.open(buffer, 'a')
-    File.foreach(stud_info).with_index do |actor, index|
-        age=actor.split[2]
-        file.puts actor if num.to_i==age.to_i
+def updateRes(num)
+    file = File.open(BUFFER, 'a')
+    File.foreach(BD_FILE).with_index do |actor, index|
+        age = actor.split[2]
+        file.puts actor if num.to_i == age.to_i
     end
     file.close
 end
 
-File.open(buffer, 'w')
-ageArray=[]
+File.open(BUFFER, 'w')
+agePut=1
+ageMassiv = []
 puts 'Введите возраст для записи подходящих людей'
-age=gets.to_i
-ageArray.push(age)
-Updates(age)
-while age!=-1
-    if buffer.size==stud_info.size
-        puts "Добавлены все люди из файла"
+agePut = gets.to_i
+ageMassiv.push(agePut)
+updateRes(agePut)
+while agePut!=-1
+    if BUFFER.size == BD_FILE.size
+        puts 'Добавлены все люди из файла'
         break
     end
-    puts "Введите возраст для записи подходящих людей"
-    age=gets.to_i
-    for num in ageArray
-        count=0
-        if num==ageArray
-            count +=1
-            puts "Введите другое число"
+    puts 'Введите возраст для записи подходящих людей'
+    agePut = gets.to_i
+    for i in ageMassiv
+        count = 0
+        if i == agePut
+            count += 1
+            puts 'Введите другое число'
             break
         end
     end
-    if count==0 && age!=-1
-        ageArray.push(age)
-        Updates(age)
+    if count == 0 && agePut != -1
+        ageMassiv.push(agePut)
+        updateRes(agePut)
     end
-    File.write(result,File.read(buffer))
-    file1=File.open("students.txt")
-    file2=File.open("results.txt")
-    if file1.size+2==file2.size
-        puts "Добавлены все люди из исходного файла"
+    File.write(RESULT, File.read(BUFFER))
+    file1 = File.open("students.txt")
+    file2 = File.open("results.txt")
+    if file1.size== file2.size
+        puts 'Добавлены все люди из исходного файла'
         break
     end
 end
-file=File.open(buffer,'w')
+file = File.open(BUFFER, 'w')
 file.close
