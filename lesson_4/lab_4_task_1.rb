4c86ad34120385cddd6151afc45629711825aaef
VadimSavintsev <161233776+VadimSavintsev@users.noreply.github.com>
2024-07-09 09:08:47 +0200

    Added lab_4_task_1.rb

---
+++ lesson_4/lab_4_task_1.rb
@@ -0,0 +1,62 @@
+#Переписать банкомат из ЛЗ #3 на работу с классами.
+#Класс должен называться -  CashMachine.
+#Программа должна запускаться с помощью метода класса init, создавать экзепляр класса и взаимодействовать с пользователем согласно условиям задачи
+
+class CashMachine
+    def self.Deposit
+        puts "Введите сумму, которую хотите внести: \n"
+        sum=gets.to_f
+        if(sum>0)
+            $balance+=sum
+            puts "Новый баланс: \n"
+            puts sprintf("%.2f",$balance)
+            return $balance
+        else
+            puts "Некорректно введена сумма! Сумма должна быть больше нуля \n"
+        end
+    end
+    def self.Withdraw
+        puts "Введите сумму, которую хотите вынести: \n"
+        sum=gets.to_f
+        if(sum>0 && sum<=$balance)
+            $balance-=sum
+            puts "Новый баланс: \n"
+            puts sprintf("%.2f",$balance)
+            return $balance
+        else
+            puts "Некорректно введена сумма! Сумма должна быть больше нуля и меньше или равно балансу \n"
+        end
+    end
+    def self.ShowBalance
+        puts "Текущий баланс: "
+        puts sprintf("%.2f",$balance)
+        return $balance
+    end
+    def init
+        print "Введите команду (D - добавить средства, W - снять средства, B - проверить баланс, Q - закрыть программу): \n"
+        command=gets.chomp
+        while command.upcase!="Q" do
+            if command.upcase=="D"
+                $balance=self.class.Deposit
+                print "Введите команду (D - добавить средства, W - снять средства, B - проверить баланс, Q - закрыть программу): \n"
+                command=gets.chomp
+            elsif command.upcase=="W"
+                $balance=self.class.Withdraw
+                print "Введите команду (D - добавить средства, W - снять средства, B - проверить баланс, Q - закрыть программу): \n"
+                command=gets.chomp
+            elsif command.upcase=="B"
+                $balance=self.class.ShowBalance
+                print "Введите команду (D - добавить средства, W - снять средства, B - проверить баланс, Q - закрыть программу): \n"
+                command=gets.chomp
+            else
+                print "Неправильно введена команда! \n"
+                print "Введите команду (D - добавить средства, W - снять средства, B - проверить баланс, Q - закрыть программу): \n"
+                command=gets.chomp
+            end
+        end
+    end
+end
+
+$balance=100.0
+cashmachine=CashMachine.new
+cashmachine.init
